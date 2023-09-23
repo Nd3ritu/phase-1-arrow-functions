b8d108d4111f82ab26d3097a1465c81543471e14
Matthew <+>
2023-09-23 13:01:59 +0300

    Completed arrow functions lab

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (num1 = 2000, num2 = 100) => {
+    return num1 / num2;} 
+divide();
+console.log (divide());
+
+const square = (x = 2) => {
+    return x**x ;
+}
+square()
+console.log (square())
+
+const add = (a = 3, b = 4) => {
+    return a + b;
+}
+add()
+console.log(add())
